662066a7c58c239099e8ab1cca0cf46b432abd9d
Danil Mustafin <danil.mustafin921@gmail.com>
2023-05-11 15:23:47 +0500

    SQLite

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -84,13 +84,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
